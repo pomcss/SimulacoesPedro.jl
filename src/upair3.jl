9537f4cda7a30704672a4af7ca6907edbdf2f80b
Selene Sofi <pomc.ecc@gmail.com>
2021-01-11 15:40:29 -0300

    Fixed formula problems

--- src/upair3.jl
+++ src/upair3.jl
@@ -11,6 +11,6 @@ function upair3(p1, p2, data)
     r12 = r6^2
     r14 = r12*r2
     u = data.ε4*(data.σ12/r12 - data.σ6/r6)
-    f = data.ε24*(2*data.σ12/r14 - data.σ6/r8) * [dx, dy]
+    f = -data.ε24*(2*data.σ12/r14 - data.σ6/r8) * [dx, dy]
     return u, f
 end
